f83dcf27f8716a889a537f5e07191ca93aa11421
Long Lê Đăng <hicongtuoc@gmail.com>
2021-09-23 20:39:23 +0700

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -4,7 +4,7 @@ function paintName(name) {
   nameContainer.innerHTML = "";
   const title = document.createElement("span");
   title.className = "name__text";
-  title.innerHTML = `Hello ${name}`;
+  title.innerHTML = `Chào ${name}`;
   nameContainer.appendChild(title);
 }
 
@@ -19,7 +19,7 @@ function handleSubmit(event) {
 
 function paintInput() {
   const input = document.createElement("input");
-  input.placeholder = "Type your name here";
+  input.placeholder = "Nhập tên của bạn tại đây";
   input.type = "text";
   input.className = "name__input";
   const form = document.createElement("form");
